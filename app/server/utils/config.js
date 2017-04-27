ee3f4fe16c3373c538a2004d01b56d7c525a6997
joshlgrossman <josh.l.grossman@gmail.com>
2017-04-27 05:54:48 -0500

    pw or password cmd line arg

--- app/server/utils/config.js
+++ app/server/utils/config.js
@@ -3,5 +3,5 @@ const args = require('./command-line-args');
 module.exports = {
 	PORT: 8001,
 	SECRET: 'secret_123',
-	DATABASE_URL: `mongodb://test:${args.pw}@ds155160.mlab.com:55160/josh3141`
+	DATABASE_URL: `mongodb://test:${args.pw || args.password}@ds155160.mlab.com:55160/josh3141`
 };
